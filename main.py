b345b1ec1df7944187816a24e7783d879e80c1a5
saviochiu <mickeyking852@gmail>
2021-10-21 17:12:11 +0800

    uploaded on 21/10/2021-17:12

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
-import requests,re
+import requests,re,json
 from bs4 import BeautifulSoup
-urls = [
-    'https://www.securityweek.com',
-    'https://www.bleepingcomputer.com',
-    'https://www.helpnetsecurity.com/view/news',
-    'https://thehackernews.com'
-]
-securityweek_news = []
 
-request = requests.get('https://www.securityweek.com')
-out = request.content
-soup = BeautifulSoup(out,'html.parser')
-strhtm = soup.prettify()
-with open('test.html','w') as f:
-    f.write(strhtm)
-    f.close()
-with open('test.html','r') as f:
-    reader = f.readlines()
-    for line in range(len(reader)):
-        if re.search('<span class="field-content">',reader[line]):
-            securityweek_news.append( reader[line+1].replace('<a href="','').replace('">','').replace(' ','').replace('\n',''))
-        else: pass
-    f.close()
-    print(securityweek_news)
+def securityweek()->list:
 
-# print(out)
+    securityweek_news = []
+
+    response = requests.get('https://www.securityweek.com')
+    soup = BeautifulSoup(response.content,"html.parser")
+
+    div_tag = soup.find_all("div",class_="view view-lastest-security view-id-lastest_security view-display-id-block_1 view-dom-id-3")
+    span_tag = div_tag[0].find_all("span",class_="field-content")
+
+    for tag in span_tag:
+        securityweek_news.append( {'Type':'security week','Title':tag.string,'Link':f'https://www.securityweek.com{tag.find_all("a")[0].get("href")}'})
+    return securityweek_news
+
+def bleepingcomputer()->list:
+
+    bleepingcomputer_news = []
+
+    response = requests.get('https://www.bleepingcomputer.com')
+    soup = BeautifulSoup(response.content, "html.parser")
+    div_tag = soup.find_all("div",class_="bc_latest_news")
+    li_tag = div_tag[0].find_all("li")
+    for tag in li_tag:
+        if tag.find_all("div",class_="bc_latest_news_category")[0].find_all("a")[0].string == 'SECURITY':
+            bleepingcomputer_news.append( {'Type':'bleeping computer','Title':tag.find_all("a")[0].string,'Link':tag.find_all("a")[0].get('href')} )
+
+    print(bleepingcomputer_news)
+
+def thehackernews()->list:
+
+    thehackernews_news = []
+
+    response = requests.get('https://thehackernews.com')
+
+bleepingcomputer()
