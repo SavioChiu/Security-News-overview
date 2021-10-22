2de9d6fe34582d1bf52ae37bdea0f00c08e7a753
saviochiu <mickeyking852@gmail.com>
2021-10-22 21:32:33 +0800

    uploaded on 20/10/2021-15:14

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import requests
+import requests, webbrowser
+import tkinter
+from tkinter import font
 from bs4 import BeautifulSoup
 
 class web_clawer:
@@ -7,12 +9,11 @@ class web_clawer:
 
         self.news = []
 
-        self.security_week()
-        self.bleeping_computer()
-        self.hacker_news()
+        self.reload()
+        self.gui()
 
-        for i in self.news:
-            print(i)
+        # for i in range(len(self.news)):
+        #     print(f"{self.news[i]['Title']} | {self.news[i]['Type']} | {self.news[i]['Time']}",end='\n<-------------------------------->\n')
 
     def security_week(self):
 
@@ -23,7 +24,14 @@ class web_clawer:
         span_tag = div_tag[0].find_all("span",class_="field-content")
 
         for tag in span_tag:
-            self.news.append( {'Type':'security week','Title':tag.string,'Link':f'https://www.securityweek.com{tag.find_all("a")[0].get("href")}'})
+            self.news.append(
+                    {
+                    'Type':'security week',
+                    'Title':tag.string,
+                        'Link':f'https://www.securityweek.com{tag.find_all("a")[0].get("href")}',
+                    'Time': ''
+                    }
+            )
 
     def bleeping_computer(self):
 
@@ -39,7 +47,8 @@ class web_clawer:
                     self.news.append(
                         {'Type': 'bleeping computer',
                          'Title': tag.find_all("h4")[0].string,
-                         'Link': tag.find_all("a")[0].get('href')
+                         'Link': tag.find_all("a")[0].get('href'),
+                         'Time': ''
                          }
                     )
 
@@ -54,8 +63,54 @@ class web_clawer:
             self.news.append(
                 {'Type': 'hacker news',
                  'Title': tag.find_all("h2")[0].string,
-                 'Link': tag.find_all("a",class_="story-link")[0].get('href')
+                 'Link': tag.find_all("a",class_="story-link")[0].get('href'),
+                 'Time':''
                  }
             )
+
+    def reload(self):
+        self.security_week()
+        self.bleeping_computer()
+        self.hacker_news()
+
+    def listbox_select(self,event):
+
+        selected = self.listbox.get(self.listbox.curselection()[0])
+
+        for i in range(0,len(self.news)):
+            if self.news[i]["Title"] in selected:
+                webbrowser.open(self.news[i]["Link"])
+                break
+            else: pass
+
+    def gui(self):
+
+        counter = 1
+
+        root = tkinter.Tk()
+        root.title('web_crawler')
+        root.geometry('1000x440')
+
+        listbox_format = '{title:^100}|{source:^20}|{time:^20}'
+        font1 = font.Font(family='consolas', size='10')
+
+        scrollbar = tkinter.Scrollbar(root)
+        self.listbox = tkinter.Listbox(root, width=200, height=25, font=font1, yscrollcommand=scrollbar.set)
+        self.listbox.bind('<ButtonRelease-1>',self.listbox_select)
+        scrollbar.config(command=self.listbox.yview)
+
+        self.listbox.insert(0, listbox_format.format(title='Title', source='Source', time='Time'))
+        for item in self.news:
+            self.listbox.insert(counter,listbox_format.format(title=item['Title'],source=item['Type'],time=item['Time']))
+            counter += 1
+
+        scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+        self.listbox.pack()
+
+        reload = tkinter.Button(root,text='Reload',command=self.reload)
+        #reload.bind('<Return>',lambda event: print('pressed F5 key'))
+        reload.pack()
+
+        root.mainloop()
 
 obj = web_clawer()
